6a725b14d15f896e637608d8d24a8d81ffc5483d
kamilaut <kamila.utyukova@gmail.com>
2022-11-30 15:24:03 +0100

    query myFlix API server’s /movies endpoint with a get request using Axios
    and added propTypes to validate the data types

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -6,39 +6,45 @@ import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
     constructor() {
-        // same code
-    }
+        super();
+        this.state = {
+            movies: [],
+            selectedMovie: null,
+            user: null,
+        }
 
-    componentDidMount() {
-        axios.get('https://mirror-stage.herokuapp.com/movies')
-            .then(response => {
-                this.setState({
-                    movies: response.data
+        componentDidMount(){
+            axios.get('https://mirror-stage.herokuapp.com')
+                .then(response => {
+                    this.setState({
+                        movies: response.data
+                    });
+                })
+                .catch(error => {
+                    console.log(error);
                 });
-            })
-            .catch(error => {
-                console.log(error);
+        }
+
+        setSelectedMovie(movie) {
+            this.setState({
+                selectedMovie: movie,
             });
+        }
+
+        render() {
+            const { movies, selectedMovie } = this.state;
+
+            if (movies.length === 0) return <div className="main-view" />;
+
+            return (
+                <div className="main-view">
+                    {selectedMovie
+                        ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+                        : movies.map(movie => (
+                            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
+                        ))
+                    }
+                </div>
+            );
+        }
     }
-
-    setSelectedMovie(newSelectedMovie) {
-        // same code
-    }
-
-    render() {
-        const { movies, selectedMovie } = this.state;
-
-        if (movies.length === 0) return <div className="main-view" />;
-
-        return (
-            <div className="main-view">
-                {selectedMovie
-                    ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-                    : movies.map(movie => (
-                        <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
-                    ))
-                }
-            </div>
-        );
-    }
-}
