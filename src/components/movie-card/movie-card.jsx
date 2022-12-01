d3fae94c5ebc348f9ee6ad51fc76c6a2b872a1f6
kamilaut <kamila.utyukova@gmail.com>
2022-12-01 15:49:56 +0100

    props for moviecard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -13,7 +13,12 @@ export class MovieCard extends React.Component {
 
 MovieCard.propTypes = {
     movie: PropTypes.shape({
-        Title: PropTypes.string
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        Genre: PropTypes.string.isRequired,
+        Director: PropTypes.string.isRequired,
+        Bio: PropTypes.string.isRequired,
+        ImagePath: PropTypes.string.isRequired
     }).isRequired,
     onMovieClick: PropTypes.func.isRequired
 };
