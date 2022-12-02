d202dff63af5a90e7e6fac7032f1f324741d3f83
kamilaut <kamila.utyukova@gmail.com>
2022-12-02 13:33:25 +0100

    added movieview proptypes

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 
 export class MovieView extends React.Component {
 
-    keypressCallback(event) {
-        console.log(event.key);
-    }
-
-    componentDidMount() {
-        document.addEventListener('keypress', this.keypressCallback);
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener('keypress', this.keypressCallback);
-    }
 
     render() {
         const { movie, onBackClick } = this.props;
@@ -41,12 +32,15 @@ export class MovieView extends React.Component {
         );
     }
 }
-export class MainView extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            movies: [],
-            selectedMovie: null
-        };
-    }
-}
+
+MovieView.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        Genre: PropTypes.string.isRequired,
+        Director: PropTypes.string.isRequired,
+        Bio: PropTypes.string.isRequired,
+        ImagePath: PropTypes.string.isRequired
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
